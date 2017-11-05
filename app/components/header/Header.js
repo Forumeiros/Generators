6247b7c72579c62b76d8b06d98bcd619ccb03113
Luiz Felipe <frazaolipe@gmail.com>
2017-11-05 13:19:44 -0200

    Atualização no webpack.

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Navbar from './Navbar.js';
 
-import './header.scss';
+import './header-styles.scss';
 
 export default class Header extends React.Component {
   render () {
