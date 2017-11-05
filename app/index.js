79c4c7b676491585880bc955b1335ef30b695ca3
Luiz Felipe <frazaolipe@gmail.com>
2017-11-05 15:10:53 -0200

    Redefinição geral. #1

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './_css/index.scss';
-
-import Generator from './components/Generator.js';
-
-ReactDOM.render(<Generator />, document.getElementById('app'));
